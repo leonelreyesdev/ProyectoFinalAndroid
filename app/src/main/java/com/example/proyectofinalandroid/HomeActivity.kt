e7ca09aed817210dae0d32aa8365218c8c1810bb
santi654 <ssanti6546541@gmail.com>
2025-09-23 22:32:08 -0600

    Tetris y Snake Implementados

--- app/src/main/java/com/example/proyectofinalandroid/HomeActivity.kt
+++ app/src/main/java/com/example/proyectofinalandroid/HomeActivity.kt
@@ -4,49 +4,53 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 import com.example.proyectofinalandroid.pacman.PacmanActivity
 import com.example.proyectofinalandroid.TicTacToe.TicTacToeActivity
 import com.example.proyectofinalandroid.Memory.MemoryGameActivity
 import com.example.proyectofinalandroid.BreakOut.BreakoutActivity
+import com.example.proyectofinalandroid.Snake.SnakeActivity
+import com.example.proyectofinalandroid.Tetris.TetrisActivity
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val username = intent.getStringExtra("username")
-        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
-        tvWelcome.text = "¡Bienvenido, $username!"
+        // Mostrar usuario
+        val username = intent.getStringExtra("username") ?: "Jugador"
+        findViewById<TextView>(R.id.tvWelcome).text = "¡Bienvenido, $username!"
 
         // Juego 1: Pac-Man
         findViewById<Button>(R.id.btnJuego1).setOnClickListener {
             startActivity(Intent(this, PacmanActivity::class.java))
         }
 
-        // Juego 2: Tres en Raya
+        // Juego 2: Tic Tac Toe
         findViewById<Button>(R.id.btnJuego2).setOnClickListener {
             startActivity(Intent(this, TicTacToeActivity::class.java))
         }
 
-        // Juego 3: Memorama
+        // Juego 3: Memory
         findViewById<Button>(R.id.btnJuego3).setOnClickListener {
             startActivity(Intent(this, MemoryGameActivity::class.java))
         }
 
-        // Juego 4: Breakout
+        // Juego 4: BreakOut
         findViewById<Button>(R.id.btnJuego4).setOnClickListener {
             startActivity(Intent(this, BreakoutActivity::class.java))
         }
 
-        // Juegos 5 a 6: aún no implementados
+        // Juego 5: Snake
         findViewById<Button>(R.id.btnJuego5).setOnClickListener {
-            Toast.makeText(this, "Juego 5 aún no disponible", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, SnakeActivity::class.java))
         }
 
+        // Juego 6: Tetris
         findViewById<Button>(R.id.btnJuego6).setOnClickListener {
-            Toast.makeText(this, "Juego 6 aún no disponible", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, TetrisActivity::class.java))
         }
     }
 }
